38524bb39a44f48a7a3ae4515712cc06820a17f9
ChangMin <sopg04@gmail.com>
2022-05-14 23:55:25 +0900

    Observable 생성 연산자

--- Rx Playground/Observable.playground/Contents.swift
+++ Rx Playground/Observable.playground/Contents.swift
@@ -75,3 +75,94 @@ Observable.range(start: 1, count: 9)
     .subscribe(onNext: {
         print("2*\($0) = \(2*$0)")
     })
+
+
+// dispose -> subscribe를 취소
+print("----dispose----")
+Observable.of(1,2,3)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .dispose()
+
+
+// disposeBag ->
+print("----disposeBag----")
+let disposeBag = DisposeBag()
+Observable.of(1,2,3)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+
+print("----create1----")
+Observable.create { observer -> Disposable in
+    observer.onNext(1)
+    observer.onCompleted()
+    observer.onNext(2)
+    return Disposables.create()
+}
+.subscribe {
+    print($0)
+}
+.disposed(by: disposeBag)
+
+print("----create2----")
+enum MyError: Error {
+    case anError
+}
+
+Observable.create { observer -> Disposable in
+    observer.onNext(1)
+    observer.onError(MyError.anError)
+    observer.onCompleted()
+    observer.onNext(2)
+    return Disposables.create()
+}
+.subscribe(
+    onNext: {
+        print($0)
+    },
+    onError: {
+        print($0.localizedDescription)
+    },
+    onCompleted: {
+        print("Completed")
+    },
+    onDisposed: {
+        print("Disposed")
+    }
+)
+.disposed(by: disposeBag)
+
+
+// deffered -> Observable 안에 Observable 생성 가능
+print("----deffered1----")
+Observable.deferred {
+    Observable.of(1,2,3)
+}
+.subscribe {
+    print($0)
+}
+.disposed(by: disposeBag)
+
+print("----deffered2----")
+var 뒤집기: Bool = false
+
+let factory: Observable<String> = Observable.deferred {
+    뒤집기 = !뒤집기
+    
+    if 뒤집기 {
+        return Observable.of("👍")
+    } else {
+        return Observable.of("👎")
+    }
+}
+
+for _ in 0...3 {
+    factory.subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+}
