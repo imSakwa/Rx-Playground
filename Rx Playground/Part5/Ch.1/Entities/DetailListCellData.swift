ac1bc9d0ab25287931ac7cc5822f2de3b91de4ec
ChangMin <sopg04@gmail.com>
2022-06-19 20:35:36 +0900

    Part 5 - Ch.1 - Entity 구현

---
+++ Rx Playground/Part5/Ch.1/Entities/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  Rx Playground
+//
+//  Created by ChangMin on 2022/06/19.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ Rx Playground/Part5/Ch.1/Entities/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  Rx Playground
+//
+//  Created by ChangMin on 2022/06/19.
+//
+
+import Foundation
+
+struct LocationData: Decodable {
+    let documents: [KLDocument]
+}
